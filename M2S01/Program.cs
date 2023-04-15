a32761319c5bd351ab2dc2c789951b046ce9d593
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-04-15 14:44:14 -0300

    Ex 01

---
+++ M2S01/Program.cs
@@ -0,0 +1,34 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bem vindo!, Selecione um exercício que deseje executar (0-10)");
+        int selecao;
+        selecao = int.Parse(Console.ReadLine());
+
+        switch (selecao)
+        {
+            case 1:
+                int num1, num2;
+                
+                Console.WriteLine("Digite dois números para somá-los!");
+                num1 = int.Parse(Console.ReadLine());
+                num2 = int.Parse(Console.ReadLine());
+
+                int soma = num1 + num2;
+                Console.WriteLine($"O resultado da soma é {soma}");
+                break;
+                
+
+            case 2:
+                Console.WriteLine("2");
+                break;
+
+
+            default:
+                Console.WriteLine("Selecione uma opÇão");
+                break;
+
+        }
+    }
+}
